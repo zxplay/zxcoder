e02f9d757f62f407cbccbabfc6e385244760f381
Steven Robertson <steverobertson@gmail.com>
2022-02-12 13:16:58 +0000

    Removing es5 output folder

--- es5/runtime/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.FRAME_BUFFER_SIZE = void 0;
-var FRAME_BUFFER_SIZE = 0x6600;
-exports.FRAME_BUFFER_SIZE = FRAME_BUFFER_SIZE;
