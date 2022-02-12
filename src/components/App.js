e1db00df52292e68d290755c99652cf68ad65068
Steven Robertson <steverobertson@gmail.com>
2022-02-12 17:52:44 +0000

    Replacing call to onBodyLoad with inline code

--- src/components/App.js
+++ src/components/App.js
@@ -5,98 +5,6 @@ import {ImageButton, SingleWindow} from "../canvasgui/canvasgui";
 export default function App() {
 
     useEffect(() => {
-        const onBodyLoad = function () {
-            const emuParams = {
-                zoom: 3,
-                sandbox: true,
-                autoLoadTapes: true,
-            };
-
-            let keystr = '1234567890,QWERTYUIOP,ASDFGHJKLe,cZXCVBNMs_';
-            // let keystr = '-W-P,ASDe,123456789M';    // snake
-            // let keystr = 'GH-e,OP-Z';    // manic miner
-            // let keystr = 'OPeZ'; // manic miner simple
-
-            let doFilter = false;
-
-            const url = new URL(window.location.href);
-            for (const [key, value] of url.searchParams) {
-                if (key === 'm') {
-                    if (value === '48' || value === '128' || value === '5') {
-                        emuParams.machine = value;
-                    }
-                } else if (key === 'k') {
-                    keystr = value;
-                } else if (key === 'u') {
-                    emuParams.openUrl = value;
-                } else if (key === 'f') {
-                    if (value && value !== '0') {
-                        doFilter = true;
-                    }
-                }
-                // console.log(key, value);
-            }
-
-            window.emu = JSSpeccy(document.getElementById('jsspeccy'), emuParams);
-            if (doFilter) {
-                document.getElementsByTagName('canvas')[0].style.imageRendering = "auto";
-            }
-
-            const width = 960;
-            let height = 0;
-
-            const btnrows = [];
-
-            const keyrows = keystr.split(',');
-            for (let j = 0; j < keyrows.length; j++) {
-                const keyrow = keyrows[j];
-                let rowlen = keyrow.length;
-                if (rowlen === 0) continue;
-                if (rowlen > 10) rowlen = 10;
-                const d = width / rowlen;
-                const btnrow = {d: d, chs: []};
-                for (let i = 0; i < rowlen; i++) {
-                    let ch = keyrow.charAt(i);
-                    if (!(ch in keyCodes)) {
-                        ch = '-';
-                    }
-                    btnrow.chs.push(ch);
-                }
-                btnrows.push(btnrow);
-                height += d;
-            }
-
-            // console.log(btnrows)
-
-            const win = new SingleWindow('virtkeys');
-            win.setTargetSize(width, height);
-
-            let x = 0;
-            let y = 0;
-            for (let j = 0; j < btnrows.length; j++) {
-                const btnrow = btnrows[j];
-                const d = btnrow.d;
-                x = 0;
-                for (let i = 0; i < btnrow.chs.length; i++) {
-                    const ch = btnrow.chs[i];
-                    if (ch === '-') {
-                        new ImageButton(win, x, y, d, d, 'img/keyNONE.png', 'img/keyNONE.png');
-                    } else {
-                        let suffix = ch;
-                        if (suffix in imgExceptions) {
-                            suffix = imgExceptions[ch];
-                        }
-                        let code = keyCodes[ch];
-                        new MyImageButton(win, x, y, d, d, suffix, code);
-                        // console.log(x, y, d, d, suffix, code);
-                    }
-                    x += d;
-                }
-                y += d;
-            }
-
-            win._onload();
-        }
 
         // _: [space]        space
         // e: [enter]        enter
@@ -151,7 +59,96 @@ export default function App() {
             document.dispatchEvent(event);
         }
 
-        onBodyLoad();
+        const emuParams = {
+            zoom: 3,
+            sandbox: true,
+            autoLoadTapes: true,
+        };
+
+        let keystr = '1234567890,QWERTYUIOP,ASDFGHJKLe,cZXCVBNMs_';
+        // let keystr = '-W-P,ASDe,123456789M';    // snake
+        // let keystr = 'GH-e,OP-Z';    // manic miner
+        // let keystr = 'OPeZ'; // manic miner simple
+
+        let doFilter = false;
+
+        const url = new URL(window.location.href);
+        for (const [key, value] of url.searchParams) {
+            if (key === 'm') {
+                if (value === '48' || value === '128' || value === '5') {
+                    emuParams.machine = value;
+                }
+            } else if (key === 'k') {
+                keystr = value;
+            } else if (key === 'u') {
+                emuParams.openUrl = value;
+            } else if (key === 'f') {
+                if (value && value !== '0') {
+                    doFilter = true;
+                }
+            }
+            // console.log(key, value);
+        }
+
+        window.emu = JSSpeccy(document.getElementById('jsspeccy'), emuParams);
+        if (doFilter) {
+            document.getElementsByTagName('canvas')[0].style.imageRendering = "auto";
+        }
+
+        const width = 960;
+        let height = 0;
+
+        const btnrows = [];
+
+        const keyrows = keystr.split(',');
+        for (let j = 0; j < keyrows.length; j++) {
+            const keyrow = keyrows[j];
+            let rowlen = keyrow.length;
+            if (rowlen === 0) continue;
+            if (rowlen > 10) rowlen = 10;
+            const d = width / rowlen;
+            const btnrow = {d: d, chs: []};
+            for (let i = 0; i < rowlen; i++) {
+                let ch = keyrow.charAt(i);
+                if (!(ch in keyCodes)) {
+                    ch = '-';
+                }
+                btnrow.chs.push(ch);
+            }
+            btnrows.push(btnrow);
+            height += d;
+        }
+
+        // console.log(btnrows)
+
+        const win = new SingleWindow('virtkeys');
+        win.setTargetSize(width, height);
+
+        let x = 0;
+        let y = 0;
+        for (let j = 0; j < btnrows.length; j++) {
+            const btnrow = btnrows[j];
+            const d = btnrow.d;
+            x = 0;
+            for (let i = 0; i < btnrow.chs.length; i++) {
+                const ch = btnrow.chs[i];
+                if (ch === '-') {
+                    new ImageButton(win, x, y, d, d, 'img/keyNONE.png', 'img/keyNONE.png');
+                } else {
+                    let suffix = ch;
+                    if (suffix in imgExceptions) {
+                        suffix = imgExceptions[ch];
+                    }
+                    let code = keyCodes[ch];
+                    new MyImageButton(win, x, y, d, d, suffix, code);
+                    // console.log(x, y, d, d, suffix, code);
+                }
+                x += d;
+            }
+            y += d;
+        }
+
+        win._onload();
     }, []);
 
     return (
