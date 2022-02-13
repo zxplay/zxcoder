7aa37e603d4998d61b7e82af05cbfc50a7972d6b
Steven Robertson <steverobertson@gmail.com>
2022-02-13 15:47:30 +0000

    Moved jsspeccy instance to sagas file

--- src/redux/reducers/jsspeccy.js
+++ src/redux/reducers/jsspeccy.js
@@ -1,105 +1,23 @@
 import {actionTypes} from "../actions/jsspeccy";
-import {JSSpeccy} from "../../lib/emulator/JSSpeccy";
 
 // -----------------------------------------------------------------------------
 // Initial state
 // -----------------------------------------------------------------------------
 
 const initialState = {
-    jsspeccy: undefined
+
 };
 
 // -----------------------------------------------------------------------------
 // Actions
 // -----------------------------------------------------------------------------
 
-function renderEmulator(state, action) {
-    const zoom = action.zoom || 2;
-    const target = action.target || document.getElementById('jsspeccy');
-
-    const emuParams = {
-        zoom,
-        sandbox: false,
-        autoLoadTapes: true,
-    };
-
-    let doFilter = false;
-
-    const url = new URL(window.location.href);
-    for (const [key, value] of url.searchParams) {
-        if (key === 'm') {
-            if (value === '48' || value === '128' || value === '5') {
-                emuParams.machine = value;
-            }
-        } else if (key === 'u') {
-            emuParams.openUrl = value;
-        } else if (key === 'f') {
-            if (value && value !== '0') {
-                doFilter = true;
-            }
-        }
-    }
-
-    const jsspeccy = JSSpeccy(target, emuParams);
-    // jsspeccy.hideUI();
-
-    if (doFilter) {
-        // TODO: Investigate this option, and narrow the element selector.
-        document.getElementsByTagName('canvas')[0].style.imageRendering = "auto";
-    }
-
-    return {...state, jsspeccy};
-}
-
-function handleClick(state, action) {
-    const target = action.e.target;
-    if (target instanceof Element) {
-        if (target instanceof HTMLInputElement ||
-            target instanceof HTMLTextAreaElement ||
-            target.classList.contains('CodeMirror-lines') ||
-            target.classList.contains('CodeMirror-line') ||
-            target.classList.contains('CodeMirror-sizer') ||
-            target.classList.contains('CodeMirror')) {
-            state.jsspeccy.pause();
-        }
-    }
-
-    // There's no change to state here.
-    return {...state};
-}
-
-function reset(state, _) {
-    state.jsspeccy.start();
-    state.jsspeccy.reset();
-    return {...state};
-}
-
-function pause(state, _) {
-    state.jsspeccy.pause();
-    return {...state};
-}
-
-function exit(state, _) {
-    state.jsspeccy.exit();
-    return {...state};
-}
-
-function showOpenFileDialog(state, _) {
-    try {state.jsspeccy.openFileDialog();} catch {};
-    return {...state};
-}
-
 // -----------------------------------------------------------------------------
 // Reducer
 // -----------------------------------------------------------------------------
 
 const actionsMap = {
-    [actionTypes.renderEmulator]: renderEmulator,
-    [actionTypes.handleClick]: handleClick,
-    [actionTypes.reset]: reset,
-    [actionTypes.pause]: pause,
-    [actionTypes.exit]: exit,
-    [actionTypes.showOpenFileDialog]: showOpenFileDialog,
+
 };
 
 export default function reducer(state = initialState, action) {
--- src/redux/sagas/jsspeccy.js
+++ src/redux/sagas/jsspeccy.js
@@ -1,10 +1,16 @@
-import {take, put, call} from "redux-saga/effects";
+import {take, takeLatest, put, call} from "redux-saga/effects";
 import {eventChannel} from "redux-saga";
 import {handleClick} from "../actions/jsspeccy";
+import {JSSpeccy} from "../../lib/emulator/JSSpeccy";
+import {actionTypes} from "../actions/jsspeccy";
 
 // -----------------------------------------------------------------------------
 // Action watchers
 // -----------------------------------------------------------------------------
+
+export function* watchForRenderEmulatorActions() {
+    yield takeLatest(actionTypes.renderEmulator, handleRenderEmulatorActions);
+}
 
 export function* watchForClickEvents() {
     const chan = yield call(getClickEventChannel);
@@ -18,9 +24,102 @@ export function* watchForClickEvents() {
     }
 }
 
+export function* watchForHandleClickActions() {
+    yield takeLatest(actionTypes.handleClick, handleClickActions);
+}
+
+export function* watchForResetActions() {
+    yield takeLatest(actionTypes.reset, handleResetActions);
+}
+
+export function* watchForPauseActions() {
+    yield takeLatest(actionTypes.pause, handlePauseActions);
+}
+
+export function* watchForExitActions() {
+    yield takeLatest(actionTypes.exit, handleExitActions);
+}
+
+export function* watchForOpenFileDialogActions() {
+    yield takeLatest(actionTypes.showOpenFileDialog, handleOpenFileDialogActions);
+}
+
 // -----------------------------------------------------------------------------
 // Action handlers
 // -----------------------------------------------------------------------------
+
+function* handleRenderEmulatorActions(action) {
+    const zoom = action.zoom || 2;
+    const target = action.target || document.getElementById('jsspeccy');
+
+    const emuParams = {
+        zoom,
+        sandbox: false,
+        autoLoadTapes: true,
+    };
+
+    let doFilter = false;
+
+    const url = new URL(window.location.href);
+    for (const [key, value] of url.searchParams) {
+        if (key === 'm') {
+            if (value === '48' || value === '128' || value === '5') {
+                emuParams.machine = value;
+            }
+        } else if (key === 'u') {
+            emuParams.openUrl = value;
+        } else if (key === 'f') {
+            if (value && value !== '0') {
+                doFilter = true;
+            }
+        }
+    }
+
+    jsspeccy = JSSpeccy(target, emuParams);
+    // jsspeccy.hideUI();
+
+    if (doFilter) {
+        // TODO: Investigate this option, and narrow the element selector.
+        document.getElementsByTagName('canvas')[0].style.imageRendering = "auto";
+    }
+}
+
+function* handleClickActions(action) {
+    const target = action.e.target;
+    if (target instanceof Element) {
+        if (target instanceof HTMLInputElement ||
+            target instanceof HTMLTextAreaElement ||
+            target.classList.contains('CodeMirror-lines') ||
+            target.classList.contains('CodeMirror-line') ||
+            target.classList.contains('CodeMirror-sizer') ||
+            target.classList.contains('CodeMirror')) {
+            jsspeccy.pause();
+        }
+    }
+}
+
+function* handleResetActions(_) {
+    jsspeccy.start();
+    jsspeccy.reset();
+}
+
+function* handlePauseActions(_) {
+    jsspeccy.pause();
+}
+
+function* handleExitActions(_) {
+    jsspeccy.exit();
+}
+
+function* handleOpenFileDialogActions(_) {
+    jsspeccy.openFileDialog();
+}
+
+// -----------------------------------------------------------------------------
+// Supporting code
+// -----------------------------------------------------------------------------
+
+let jsspeccy = undefined;
 
 function getClickEventChannel() {
     return eventChannel(emit => {
