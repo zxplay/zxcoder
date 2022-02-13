198e88ee5241c15e5e43731c23f2d6889a0afab7
Steven Robertson <steverobertson@gmail.com>
2022-02-13 19:33:45 +0000

    Added bas2tap and test

---
+++ src/lib/bas2tap.js
@@ -0,0 +1,11 @@
+import Module from "bas2tap";
+
+export function bas2tap(input) {
+    return new Promise((resolve, reject) => {
+        Module({
+            'input': input,
+            'resolve': resolve,
+            'arguments': ['input.bas', 'output.tap', '-a', '-sbas2tap']
+        });
+    });
+}
