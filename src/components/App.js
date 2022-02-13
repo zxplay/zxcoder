43cddf83fbd15a43696a006b37df05d3aebf7225
Steven Robertson <steverobertson@gmail.com>
2022-02-13 13:14:46 +0000

    Menubar with logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,174 @@
-import React from "react";
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+import React, {Fragment} from "react";
+import "primereact/resources/themes/md-dark-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import {Emulator} from "./Emulator";
 import {BasicEditor} from "./BasicEditor";
+import {Menubar} from "primereact/menubar";
+import {InputText} from "primereact/inputtext";
 
 export default function App() {
     const zoom = 2;
     const width = zoom * 320;
+
+    const start = (
+        <img alt="logo"
+             src="/img/logo.png"
+             onError={(e) =>
+                 e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'}
+             height="40"
+             className="mr-2"
+        />
+    )
+
+    const end = <InputText placeholder="Search" type="text"/>;
+
     return (
-        <div className="grid" style={{width: "100%", padding: "4px", margin: 0}}>
-            <div className="col" style={{padding: "4px"}}>
-                <BasicEditor/>
+        <Fragment>
+            <Menubar model={items} start={start} end={end}/>
+            <div className="grid"
+                 style={{width: "100%", padding: "4px", margin: 0}}>
+                <div className="col" style={{padding: "4px"}}>
+                    <BasicEditor/>
+                </div>
+                <div className="col-fixed"
+                     style={{width: `${width + 8}px`, padding: "4px"}}>
+                    <Emulator zoom={zoom} width={width}/>
+                </div>
+                <div className="col" style={{padding: "4px"}}>
+
+                </div>
             </div>
-            <div className="col-fixed" style={{width: `${width + 8}px`, padding: "4px"}}>
-                <Emulator zoom={zoom} width={width}/>
-            </div>
-        </div>
+        </Fragment>
     )
 }
+
+const items = [
+    {
+        label: 'File',
+        icon: 'pi pi-fw pi-file',
+        items: [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+                items: [
+                    {
+                        label: 'Bookmark',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+                    {
+                        label: 'Video',
+                        icon: 'pi pi-fw pi-video'
+                    },
+
+                ]
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-trash'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'Export',
+                icon: 'pi pi-fw pi-external-link'
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+            {
+                label: 'Left',
+                icon: 'pi pi-fw pi-align-left'
+            },
+            {
+                label: 'Right',
+                icon: 'pi pi-fw pi-align-right'
+            },
+            {
+                label: 'Center',
+                icon: 'pi pi-fw pi-align-center'
+            },
+            {
+                label: 'Justify',
+                icon: 'pi pi-fw pi-align-justify'
+            },
+
+        ]
+    },
+    {
+        label: 'Users',
+        icon: 'pi pi-fw pi-user',
+        items: [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-user-plus',
+
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-user-minus',
+
+            },
+            {
+                label: 'Search',
+                icon: 'pi pi-fw pi-users',
+                items: [
+                    {
+                        label: 'Filter',
+                        icon: 'pi pi-fw pi-filter',
+                        items: [
+                            {
+                                label: 'Print',
+                                icon: 'pi pi-fw pi-print'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'pi pi-fw pi-bars',
+                        label: 'List'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Events',
+        icon: 'pi pi-fw pi-calendar',
+        items: [
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {
+                        label: 'Save',
+                        icon: 'pi pi-fw pi-calendar-plus'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    },
+
+                ]
+            },
+            {
+                label: 'Archieve',
+                icon: 'pi pi-fw pi-calendar-times',
+                items: [
+                    {
+                        label: 'Remove',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off'
+    }
+];
