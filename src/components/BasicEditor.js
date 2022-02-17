86a54a7cb593e40cc13c842da2b228f654b6c39b
Steven Robertson <steverobertson@gmail.com>
2022-02-17 09:07:05 +0000

    Full-height basic text editor

--- src/components/BasicEditor.js
+++ src/components/BasicEditor.js
@@ -11,20 +11,9 @@ export function BasicEditor(props) {
     const cmRef = useRef(null);
 
     const options = {
-        lineWrapping: true,
         readOnly: false,
-        // mode: 'formula',
         theme: 'default',
-        scrollbarStyle: 'null',
-        lineNumbers: false,
-        matchBrackets: true,
-        extraKeys: {
-            // 'Enter': () => props.onEnter && props.onEnter(this.text),
-            // 'Shift-Enter': () => props.onShiftEnter && props.onShiftEnter(this.text),
-            // 'Esc': () => props.onEscape && props.onEscape(),
-            // 'Tab': () => props.onTab && props.onTab(this.text),
-            // 'Shift-Tab': () => props.onShiftTab && props.onShiftTab(this.text),
-        }
+        matchBrackets: true
     };
 
     useEffect(() => {
