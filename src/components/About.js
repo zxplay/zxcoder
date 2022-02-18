f9cd03ec15f98e27aeb8067ef2fdb9be99d331c6
Steven Robertson <steverobertson@gmail.com>
2022-02-18 01:36:16 +0000

    Added content to the About page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,23 @@ import {Card} from "primereact/card";
 export function About() {
     return (
         <Card className="m-2">
-            About page
+            <h1>About This Website</h1>
+            <p>
+                A ZX Spectrum emulator & programming environment for the browser.
+            </p>
+            <p>
+                This site is a work in progress.
+                Source available <a href="https://github.com/stever/zxplay" target="_blank">here</a>.
+            </p>
+            <p>
+                Based on <a href="https://github.com/gasman/jsspeccy3" target="_blank">JSSpeccy3</a>.
+            </p>
+            <h2>Sinclair ROM Copyright Permission</h2>
+            <blockquote>
+                Amstrad have kindly given their permission for the
+                redistribution of their copyrighted material but retain that copyright.
+            </blockquote>
+            <a href="https://worldofspectrum.net/assets/amstrad-roms.txt" target="_blank">comp.sys.sinclair</a> 1999-08-31
         </Card>
     )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,6 @@ import "primereact/resources/themes/md-dark-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-import Constants from "../constants";
-import Config from "../../config.json";
 import {Nav} from "./Nav";
 import {MainCode} from "./MainCode";
 import {About} from "./About";
@@ -18,14 +16,6 @@ export default function App() {
                 <Route path="/" element={<MainCode/>} />
                 <Route path="/about" element={<About/>} />
             </Routes>
-            {!Constants.isDev &&
-                <script
-                    defer
-                    data-domain={Config.domain}
-                    data-api="/plausible/api/event"
-                    src="/plausible/js/script.js"
-                />
-            }
         </Fragment>
     )
 }
