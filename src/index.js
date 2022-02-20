d93837656ce171a6a6ef2121817080b74ee44ed4
Steven Robertson <steverobertson@gmail.com>
2022-02-20 11:22:00 +0000

    Changed from hash router to browser router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider as ReduxProvider} from "react-redux";
-import {HashRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import {store} from "./redux/store";
 import App from "./components/App";
 
