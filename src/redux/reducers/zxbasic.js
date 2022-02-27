ccdffa6bc3bb6644d81dddddde975b18f6d769c2
Steven Robertson <steverobertson@gmail.com>
2022-02-27 10:42:28 +0000

    Adding basic code examples

--- src/redux/reducers/zxbasic.js
+++ src/redux/reducers/zxbasic.js
@@ -5,9 +5,24 @@ import {actionTypes} from "../actions/zxbasic";
 // -----------------------------------------------------------------------------
 
 const initialState = {
-    basicCode:
-        '10 CLS\n' +
-        '20 PRINT "HELLO WORLD!"\n'
+    basicCode: `REM From the ZX Spectrum 48K Manual
+
+DIM m, n, c AS BYTE
+
+FOR m = 0 TO 1: BRIGHT m
+FOR n = 1 TO 10
+FOR c = 0 TO 7
+PAPER c: PRINT "    ";: REM 4 coloured spaces
+NEXT c: NEXT n: NEXT m
+
+FOR m = 0 TO 1: BRIGHT m: PAPER 7
+FOR c = 0 TO 3
+INK c: PRINT c; "   ";
+NEXT c: PAPER 0
+FOR c = 4 TO 7
+INK c: PRINT c; "   ";
+NEXT c: NEXT m
+PAPER 7: INK 0: BRIGHT 0`
 };
 
 // -----------------------------------------------------------------------------
