7c214872c023cd88df15aa069cac251338f1ac3e
Steven Robertson <steverobertson@gmail.com>
2022-03-03 21:22:28 +0000

    Adding plausible script tag

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import {store, history} from "./redux/store";
 import LoadingScreen from "./components/LoadingScreen";
 import LockScreen from "./components/LockScreen";
 import App from "./components/App";
+import Constants from "./constants";
+import Config from "../config.json";
 
 ReactDOM.render(
     <ReduxProvider store={store}>
@@ -14,6 +16,14 @@ ReactDOM.render(
             <LockScreen/>
             <App/>
         </ConnectedRouter>
+        {!Constants.isDev &&
+            <script
+                defer
+                data-domain={Config.domain}
+                data-api="/plausible/api/event"
+                src="/plausible/js/script.js"
+            />
+        }
     </ReduxProvider>,
     document.getElementById('root')
 );
