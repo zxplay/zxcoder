6c8c68a9db4dd874a839a60c0537a4c340cd9af3
Steven Robertson <steverobertson@gmail.com>
2022-03-04 16:12:09 +0000

    Moved Redux and Router into App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, {Fragment, useEffect} from "react";
+import {Provider as ReduxProvider} from "react-redux";
 import {Switch, Route} from "react-router-dom";
+import {ConnectedRouter} from "connected-react-router";
 import {useDispatch, useSelector} from "react-redux";
 import "primereact/resources/themes/md-dark-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import {store, history} from "../redux/store";
+import LoadingScreen from "../components/LoadingScreen";
+import LockScreen from "../components/LockScreen";
 import MaxWidth from "./MaxWidth";
 import Nav from "./Nav";
 import Demo from "./Demo";
@@ -18,48 +23,18 @@ import NotFound from "./NotFound";
 import {exit, renderEmulator} from "../redux/actions/jsspeccy";
 
 export default function App() {
-    const projectType = useSelector(state => state?.project.type);
-    const projectReady = useSelector(state => state?.project.ready);
-
-    console.log({projectType, projectReady});
-
     return (
-        <div className="pb-1">
-            <RenderEmulator/>
-            <Nav/>
-            <Switch>
-                <Route exact path="/">
-                    {!projectType &&
-                        <Demo/>
-                    }
-                    {projectType && !projectReady &&
-                        <MaxWidth>
-                            <NewProject/>
-                        </MaxWidth>
-                    }
-                    {projectType && projectReady &&
-                        <Project/>
-                    }
-                </Route>
-                <Route exact path="/about">
-                    <MaxWidth>
-                        <About/>
-                    </MaxWidth>
-                </Route>
-                <Route exact path="/profile">
-                    <YourProfile/>
-                </Route>
-                <Route exact path="/projects">
-                    <YourProjects/>
-                </Route>
-                <Route path="/search">
-                    <Search/>
-                </Route>
-                <Route>
-                    <NotFound/>
-                </Route>
-            </Switch>
-        </div>
+        <ReduxProvider store={store}>
+            <ConnectedRouter history={history}>
+                <LoadingScreen/>
+                <LockScreen/>
+                <div className="pb-1">
+                    <RenderEmulator/>
+                    <Nav/>
+                    <Routes/>
+                </div>
+            </ConnectedRouter>
+        </ReduxProvider>
     )
 }
 
@@ -75,5 +50,44 @@ function RenderEmulator() {
 
     return (
         <Fragment/>
+    )
+}
+
+function Routes() {
+    const projectType = useSelector(state => state?.project.type);
+    const projectReady = useSelector(state => state?.project.ready);
+    return (
+        <Switch>
+            <Route exact path="/">
+                {!projectType &&
+                    <Demo/>
+                }
+                {projectType && !projectReady &&
+                    <MaxWidth>
+                        <NewProject/>
+                    </MaxWidth>
+                }
+                {projectType && projectReady &&
+                    <Project/>
+                }
+            </Route>
+            <Route exact path="/about">
+                <MaxWidth>
+                    <About/>
+                </MaxWidth>
+            </Route>
+            <Route exact path="/profile">
+                <YourProfile/>
+            </Route>
+            <Route exact path="/projects">
+                <YourProjects/>
+            </Route>
+            <Route path="/search">
+                <Search/>
+            </Route>
+            <Route>
+                <NotFound/>
+            </Route>
+        </Switch>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider as ReduxProvider} from "react-redux";
-import {ConnectedRouter} from "connected-react-router";
-import {store, history} from "./redux/store";
-import LoadingScreen from "./components/LoadingScreen";
-import LockScreen from "./components/LockScreen";
 import App from "./components/App";
 
 ReactDOM.render(
-    <ReduxProvider store={store}>
-        <ConnectedRouter history={history}>
-            <LoadingScreen/>
-            <LockScreen/>
-            <App/>
-        </ConnectedRouter>
-    </ReduxProvider>,
+    <App/>,
     document.getElementById('root')
 );
