a4d5266ec33e2bcce87901830eccfca26c9b3417
Steven Robertson <steverobertson@gmail.com>
2022-03-05 14:46:03 +0000

    Hide header when loading external tape images

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import {Provider as ReduxProvider} from "react-redux";
-import {ConnectedRouter} from "connected-react-router";
+import React, {Fragment} from "react";
+import {useSelector} from "react-redux";
+import queryString from "query-string";
 import "primereact/resources/themes/md-dark-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-import {store, history} from "../redux/store";
 import RenderEmulator from "../components/RenderEmulator";
 import LoadingScreen from "../components/LoadingScreen";
 import LockScreen from "../components/LockScreen";
@@ -13,17 +12,23 @@ import Nav from "./Nav";
 import Routes from "./Routes";
 
 export default function App() {
+
+    // Hide tabs when loading external tape files.
+    const search = useSelector(state => state?.router.location.search);
+    const parsed = queryString.parse(search);
+    const externalLoad = typeof parsed.u !== 'undefined';
+
     return (
-        <ReduxProvider store={store}>
-            <ConnectedRouter history={history}>
-                <RenderEmulator/>
-                <LoadingScreen/>
-                <LockScreen/>
-                <div className="pb-1">
+        <Fragment>
+            <RenderEmulator/>
+            <LoadingScreen/>
+            <LockScreen/>
+            <div className="pb-1">
+                {!externalLoad &&
                     <Nav/>
-                    <Routes/>
-                </div>
-            </ConnectedRouter>
-        </ReduxProvider>
+                }
+                <Routes/>
+            </div>
+        </Fragment>
     )
 }
--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
+import queryString from "query-string";
 import {TabPanel, TabView} from "primereact/tabview";
 import {DemoSinclairBasicEditor} from "./DemoSinclairBasicEditor";
 import {DemoAssemblyEditor} from "./DemoAssemblyEditor";
@@ -13,6 +14,11 @@ export default function Demo() {
     const zoom = 2;
     const width = zoom * 320;
 
+    // Hide tabs when loading external tape files.
+    const search = useSelector(state => state?.router.location.search);
+    const parsed = queryString.parse(search);
+    const externalLoad = typeof parsed.u !== 'undefined';
+
     return (
         <div className="grid"
              style={{width: "100%", padding: 0, margin: 0}}>
@@ -20,22 +26,27 @@ export default function Demo() {
 
             </div>
             <div className="col-fixed" style={{width: `${width}px`, padding: '6px 0 0 0'}}>
-                <TabView
-                    activeIndex={activeIndex}
-                    onTabChange={(e) => dispatch(setSelectedTabIndex(e.index))}>
-                    <TabPanel header="Emulator">
-                        <Emulator zoom={zoom} width={width}/>
-                    </TabPanel>
-                    <TabPanel header="Z80 Assembler">
-                        <DemoAssemblyEditor/>
-                    </TabPanel>
-                    <TabPanel header="Sinclair BASIC">
-                        <DemoSinclairBasicEditor/>
-                    </TabPanel>
-                    <TabPanel header="Boriel ZX BASIC">
-                        <DemoZXBasicEditor/>
-                    </TabPanel>
-                </TabView>
+                {externalLoad &&
+                    <Emulator zoom={zoom} width={width}/>
+                }
+                {!externalLoad &&
+                    <TabView
+                        activeIndex={activeIndex}
+                        onTabChange={(e) => dispatch(setSelectedTabIndex(e.index))}>
+                        <TabPanel header="Emulator">
+                            <Emulator zoom={zoom} width={width}/>
+                        </TabPanel>
+                        <TabPanel header="Z80 Assembler">
+                            <DemoAssemblyEditor/>
+                        </TabPanel>
+                        <TabPanel header="Sinclair BASIC">
+                            <DemoSinclairBasicEditor/>
+                        </TabPanel>
+                        <TabPanel header="Boriel ZX BASIC">
+                            <DemoZXBasicEditor/>
+                        </TabPanel>
+                    </TabView>
+                }
             </div>
             <div className="col" style={{padding: 0}}>
 
