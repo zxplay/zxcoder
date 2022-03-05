37ba1258457684023db2e005f55308a544f26226
Steven Robertson <steverobertson@gmail.com>
2022-03-05 11:26:31 +0000

    Moved routes to new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,26 @@
 import React, {Fragment, useEffect} from "react";
 import {Provider as ReduxProvider} from "react-redux";
-import {Switch, Route} from "react-router-dom";
 import {ConnectedRouter} from "connected-react-router";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import "primereact/resources/themes/md-dark-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-import {store, history} from "../redux/store";
 import LoadingScreen from "../components/LoadingScreen";
 import LockScreen from "../components/LockScreen";
-import MaxWidth from "./MaxWidth";
 import Nav from "./Nav";
-import Demo from "./Demo";
-import Project from "./Project";
-import NewProject from "./NewProject";
-import YourProjects from "./YourProjects";
-import YourProfile from "./YourProfile";
-import About from "./About";
-import Search from "./Search";
-import NotFound from "./NotFound";
+import Routes from "./Routes";
+import {store, history} from "../redux/store";
 import {exit, renderEmulator} from "../redux/actions/jsspeccy";
 
 export default function App() {
     return (
         <ReduxProvider store={store}>
             <ConnectedRouter history={history}>
+                <RenderEmulator/>
                 <LoadingScreen/>
                 <LockScreen/>
                 <div className="pb-1">
-                    <RenderEmulator/>
                     <Nav/>
                     <Routes/>
                 </div>
@@ -50,44 +41,5 @@ function RenderEmulator() {
 
     return (
         <Fragment/>
-    )
-}
-
-function Routes() {
-    const projectType = useSelector(state => state?.project.type);
-    const projectReady = useSelector(state => state?.project.ready);
-    return (
-        <Switch>
-            <Route exact path="/">
-                {!projectType &&
-                    <Demo/>
-                }
-                {projectType && !projectReady &&
-                    <MaxWidth>
-                        <NewProject/>
-                    </MaxWidth>
-                }
-                {projectType && projectReady &&
-                    <Project/>
-                }
-            </Route>
-            <Route exact path="/about">
-                <MaxWidth>
-                    <About/>
-                </MaxWidth>
-            </Route>
-            <Route exact path="/profile">
-                <YourProfile/>
-            </Route>
-            <Route exact path="/projects">
-                <YourProjects/>
-            </Route>
-            <Route path="/search">
-                <Search/>
-            </Route>
-            <Route>
-                <NotFound/>
-            </Route>
-        </Switch>
     )
 }
---
+++ src/components/Routes.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {useSelector} from "react-redux";
+import {Route, Switch} from "react-router-dom";
+import Demo from "./Demo";
+import MaxWidth from "./MaxWidth";
+import NewProject from "./NewProject";
+import Project from "./Project";
+import About from "./About";
+import YourProfile from "./YourProfile";
+import YourProjects from "./YourProjects";
+import Search from "./Search";
+import NotFound from "./NotFound";
+
+export default function Routes() {
+    const projectType = useSelector(state => state?.project.type);
+    const projectReady = useSelector(state => state?.project.ready);
+    return (
+        <Switch>
+            <Route exact path="/">
+                {!projectType &&
+                    <Demo/>
+                }
+                {projectType && !projectReady &&
+                    <MaxWidth>
+                        <NewProject/>
+                    </MaxWidth>
+                }
+                {projectType && projectReady &&
+                    <Project/>
+                }
+            </Route>
+            <Route exact path="/about">
+                <MaxWidth>
+                    <About/>
+                </MaxWidth>
+            </Route>
+            <Route exact path="/profile">
+                <YourProfile/>
+            </Route>
+            <Route exact path="/projects">
+                <YourProjects/>
+            </Route>
+            <Route path="/search">
+                <Search/>
+            </Route>
+            <Route>
+                <NotFound/>
+            </Route>
+        </Switch>
+    )
+}
