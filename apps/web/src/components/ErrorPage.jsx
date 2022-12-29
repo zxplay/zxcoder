0ab0cdb40d026a13ddb2c359ac1d0963eeade3d9
Steven Robertson <stever@hey.com>
2022-12-29 22:55:08 +0000

    Adding title to error page

--- apps/web/src/components/ErrorPage.jsx
+++ apps/web/src/components/ErrorPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Titled} from "react-titled";
 import {Card} from "primereact/card";
 import {Button} from "primereact/button";
+import {sep} from "../constants";
 
 export default function ErrorPage(props) {
     let msg;
@@ -14,17 +16,19 @@ export default function ErrorPage(props) {
     }
 
     return (
-        <Card className="m-2" style={{textAlign: 'center'}}>
-            <div className="m-4">
-                {msg}
-            </div>
-            <Button
-                type="button"
-                className="p-button-text"
-                label="GO BACK TO DASHBOARD"
-                onClick={() => window.location = '/'}
-            />
-        </Card>
+        <Titled title={(s) => `Error ${sep} ${s}`}>
+            <Card className="m-2" style={{textAlign: 'center'}}>
+                <div className="m-4">
+                    {msg}
+                </div>
+                <Button
+                    type="button"
+                    className="p-button-text"
+                    label="GO BACK TO DASHBOARD"
+                    onClick={() => window.location = '/'}
+                />
+            </Card>
+        </Titled>
     )
 }
 
