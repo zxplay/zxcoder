88be71cdbc052733bc960ab5b73069eaa9e5230b
Steven Robertson <stever@hey.com>
2022-12-30 01:53:23 +0000

    Revert "Removing redundant import"
    This reverts commit 7ab7c3824ff575c01d2f40ad16936755d4b0834a.

--- apps/web/src/errors.js
+++ apps/web/src/errors.js
@@ -1,5 +1,6 @@
 import {error} from "./redux/error/actions";
 import {store} from "./redux/store";
+import React from "react";
 import {dashboardUnlock} from "./dashboard_lock";
 
 export function handleError(title, data) {
